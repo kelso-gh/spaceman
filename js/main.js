3753124d316f8a44d65c275f9f26781692e64259
kelso-gh <soyouwontsingg@gmail.com>
2022-10-07 18:02:11 -0400

    update

---
+++ js/main.js
@@ -0,0 +1,16 @@
+/*----- constants -----*/
+const words [];
+
+
+/*---- state variables ----*/
+let scores;
+let results; 
+let winner;
+
+
+/*-------- stored -------*/
+
+
+
+
+/*------- functions ------*/
