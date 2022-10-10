7e2c83b8c248fc69a20c218665cd03e4d2107846
kelso-gh <soyouwontsingg@gmail.com>
2022-10-10 10:56:28 -0400

    update

--- js/main.js
+++ js/main.js
@@ -1,28 +1,29 @@
 /*----- constants -----*/
 const words = [
-    spaceship,
-    asteroid,
-    mars,
-    venus,
-    jupiter
+    'spaceship',
+    'asteroid',
+    'mars',
+    'venus',
+    'jupiter'
 ];
 
-
+const maxWrong = 6;
 
 /*---- state variables ----*/
 let randomWord;
 let answer;
-let maxWrong; 
 let wrongGuesses; 
 
 
 
 /*-------- stored elements-------*/
-const message = document.querySelector('');
+// const message = document.querySelector('');
+const livesEl = document.getElementById('lives');
+const playerguess = document.getElementById('playerguess');
 
-/*------ event listeners ------*/
-document.querySelector('stage').addEventListener('click');
-document.querySelector('startBtn').addEventListener('click');
+// /*------ event listeners ------*/
+// document.querySelector('stage').addEventListener('click');
+// document.querySelector('startBtn').addEventListener('click');
 
 
 /*------- functions ------*/
@@ -30,10 +31,21 @@ document.querySelector('startBtn').addEventListener('click');
 initialize();
 
 function initialize() {
-    randomWord = 0;
-    answer = '';
-    maxWrong = 6;
+    const minimumIdx = 0;
+    const maxIdx = words.length - 1;
+    const randomNum = Math.floor(Math.random() * maxIdx);
+    randomWord = words[randomNum];
+    console.log(randomWord);
+    answer = randomWord.split('').map(_ => '_');
     wrongGuesses = 0;
-      
-    render();
+    livesEl.innerText = maxWrong - wrongGuesses;
+    for (let index = 0; index < answer.length; index++) {
+        const letter = answer[index];
+        const letterEl = document.createElement('span');
+        letterEl.innerText = letter;
+        playerguess.appendChild(letterEl);
+    } 
+    // render();
 };
+
+
